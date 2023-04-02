61028d94b334085ee67db21b2257936b1ac1487d
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-04-02 14:01:52 -0300

    TailwindInit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div class="container mx-auto mt-5"> 
+        <button class="
+        bg-blue-500 
+        hover:bg-green-700 
+        text-white 
+        font-bold 
+        py-4 px-8  
+        rounded"     
         >
-          Learn React
-        </a>
-      </header>
+        Mi boton
+        </button>
+      </div>
+
+<div class="mt-5">
+    <h1 class=" mt-5  text-green-500 text-xl text-center">Esto es un H1</h1>
+    <h2 class="text-right">Esto es un H2</h2>
+    <h3 class=" text-red-800 ">Esto es un H3</h3>
+    <h4 class="uppercase text-xl shadow-md">Esto es un H4</h4>
+    <h5 class="">Esto es un H5</h5>
+    <h6 class="text-white bg-blue-500">Esto es un H6</h6>
+</div>
+
+
     </div>
   );
 }
