6e2100225ff3288f98086b9064793de3448f99da
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-04-03 01:33:24 -0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -25,6 +25,26 @@ function App() {
     <h6 class="text-white bg-blue-500">Esto es un H6</h6>
 </div>
 
+<hr></hr>
+<div class="grid grid-cols-3 gap-4 my-2">
+    <div class="bg-red-400">A</div>
+    <div class="bg-red-500">B</div>
+    <div class="bg-red-600">C</div>
+    <div class="bg-red-900">D</div>
+    <div class="bg-red-800">E</div>
+    <div class="bg-red-200">F</div>
+</div>
+
+<div class="grid grid-cols-4 gap-4 my-2">
+    <div class="bg-green-400">A</div>
+    <div class="bg-green-500">B</div>
+    <div class="bg-green-600">C</div>
+    <div class="bg-green-900">D</div>
+</div>
+
+
+
+
 
     </div>
   );
