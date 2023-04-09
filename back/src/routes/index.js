ed1ddd29053ff728dc6724d8dcea11c1fc3a76da
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-04-09 11:22:08 -0300

    12312

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,16 +2,12 @@ const { Router} = require("express")
 const router = Router()
 const bodyparser = require('body-parser');
 
-
-
 const getCharById = require("../controllers/getCharById")
 const getCharDetail = require("../controllers/getCharDetail")
 const getChars = require("../controllers/getChars")
 const createRecipe = require("./createRecipe")
 
-
 router.use(bodyparser.json());
-
 
 router.use("/onsearch/:id", getCharById)
 router.use("/detail/:id", getCharDetail)
